f1dec562f9d52e77f89619165b4360f462eb597e
minixz <minikuz@gmail.com>
2021-05-02 21:07:10 +0800

    feat: add GitHub button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <router-view />
-  <footer class="AppFooter">
-    <p>Art and website develop by CocoaCaa.</p>
-    <p>
-      <a href="https://animalroyale.com/" target="_blank">Super Animal Royale</a>
-      is copyright
-      <a href="https://pixilestudios.com/" target="_blank">Pixile, Inc</a>
-      and is not affiliated with this site.
-    </p>
-  </footer>
+  <div>
+    <GitHubButton />
+    <router-view />
+    <footer class="AppFooter">
+      <p>Art and website develop by CocoaCaa.</p>
+      <p>
+        <a href="https://animalroyale.com/" target="_blank">Super Animal Royale</a>
+        is copyright
+        <a href="https://pixilestudios.com/" target="_blank">Pixile, Inc</a>
+        and is not affiliated with this site.
+      </p>
+    </footer>
+  </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import GitHubButton from '@/components/GitHubButton.vue';
+
+@Options({
+  components: {
+    GitHubButton,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import '~normalize.css';
